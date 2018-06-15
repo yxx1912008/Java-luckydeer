2e92fcefdeb4de209182bfcc8272e30a4f961124
yuanxx <824264439@qq.com>
2018-06-15 14:04:19 +0800

    自己实现的注解，用于在方法执行之前，忽略Token验证

--- luckydeer-baseaction/src/main/java/cn/luckydeer/baseaction/interceptor/AuthorizationInterceptor.java
+++ luckydeer-baseaction/src/main/java/cn/luckydeer/baseaction/interceptor/AuthorizationInterceptor.java
@@ -7,6 +7,10 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 /**
  * Token校验
+ * 针对制定的请求进行校验
+ * 如果请求想要不通过校验
+ * 可以通过自己写的注解
+ * 来进行忽略校验的请求
  * 
  * @author yuanxx
  * @version $Id: AuthorizationInterceptor.java, v 0.1 2018年6月15日 下午1:51:28 yuanxx Exp $
@@ -16,14 +20,10 @@ public class AuthorizationInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
                              Object handler) throws Exception {
-        
+
         System.out.println("方法预校验");
-        
+
         return super.preHandle(request, response, handler);
     }
 
-    
-    
-    
-    
 }
