c1782f48521ab2982284ea07402a947adc49c054
yuanxx <824264439@qq.com>
2018-06-20 11:38:36 +0800

    2018年6月20日11:38:34

--- luckydeer-dao/src/main/java/cn/luckydeer/dao/user/dataobject/SysUserDo.java
+++ luckydeer-dao/src/main/java/cn/luckydeer/dao/user/dataobject/SysUserDo.java
@@ -9,26 +9,28 @@ import java.util.Date;
  * @version $Id: SysUserDo.java, v 0.1 2018年6月20日 上午11:25:31 yuanxx Exp $
  */
 public class SysUserDo {
+
+    /** 用户ID  */
     private String  userId;
-
+    /** 昵称  */
     private String  nickName;
-
+    /**  手机号码 */
     private String  phone;
-
+    /** 头像地址  */
     private String  headPicUrl;
-
+    /**  微信openid */
     private String  openId;
-
+    /** 微信联合id  */
     private String  unionId;
-
+    /** 注册时间  */
     private Date    regTime;
-
+    /**  更新时间 */
     private Date    updateTime;
-
+    /**  用户状态 */
     private Integer userStatus;
-
+    /**  注册渠道 */
     private String  regChanel;
-
+    /**  分组ID */
     private Integer groupId;
 
     public String getUserId() {
