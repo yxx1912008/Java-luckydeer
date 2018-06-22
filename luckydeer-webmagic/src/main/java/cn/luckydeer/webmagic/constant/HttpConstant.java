6a7e76d106e073a9ea2d6b495b261e135776f538
yuanxx <824264439@qq.com>
2018-06-22 09:25:12 +0800

    增加WebMagic爬虫框架

---
+++ luckydeer-webmagic/src/main/java/cn/luckydeer/webmagic/constant/HttpConstant.java
@@ -0,0 +1,14 @@
+package cn.luckydeer.webmagic.constant;
+
+/**
+ * Http请求常量
+ * 
+ * @author yuanxx
+ * @version $Id: HttpConstant.java, v 0.1 2018年6月22日 上午9:19:04 yuanxx Exp $
+ */
+public interface HttpConstant {
+
+    //购物猫网站网址
+    String CAT_HOST = "http://star0393.com";
+
+}
