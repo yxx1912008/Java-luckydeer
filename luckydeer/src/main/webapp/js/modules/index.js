71a88ca6873efcdd0d9cde05b64d89e78bfbf685
yuanxx <824264439@qq.com>
2018-07-04 11:40:08 +0800

    测试layui

---
+++ luckydeer/src/main/webapp/js/modules/index.js
@@ -0,0 +1,10 @@
+/* 项目主入口*/
+layui.define(['layer', 'form'], function (exports) {
+    var layer = layui.layer, form = layui.form;
+    var device = layui.device();
+    layer.msg('首页');
+    exports('index', {});
+});
+
+
+
